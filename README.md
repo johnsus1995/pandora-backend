72d59acae4a881a9a5439df6c1ac1c267d7e0349
Jaison <jaisonjohny26@gmail.com>
2022-12-23 22:43:12 +0530

    fix bcrypt error and untrack env file

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # game-store-backend
+# nodejs - 
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const connectToDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
   } catch (error) {
+    // console.log(error)
     throw error;
   }
 };
